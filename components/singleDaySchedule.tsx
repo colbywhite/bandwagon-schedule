695497a785fb3ce5c74a39ee09d594fefd081463
Colby M. White <colbywhite@users.noreply.github.com>
2022-08-08 00:36:32 -0500

    css: only 3 cols for lg screens

--- components/singleDaySchedule.tsx
+++ components/singleDaySchedule.tsx
@@ -15,7 +15,7 @@ export default function SingleDaySchedule({date, games}: DayProps & GamesProps) 
   return (
     <section className="flex flex-col gap-3">
       <Typography variant="h4">{date.toFormat('cccc, LLL d')}</Typography>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 justify-between">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 justify-between">
         {games.map(game => (<GameCard game={game} key={game.id}/>))}
       </div>
     </section>
