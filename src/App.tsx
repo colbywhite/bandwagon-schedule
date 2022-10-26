b2d5b268a1d5bfb8253657f122e012294f54335e
Colby M. White <colbywhite@users.noreply.github.com>
2022-10-26 00:04:03 -0500

    fix: temp rm tests; force typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import data from './data.json';
-import type {Game, Schedule} from '../@types';
+import {gamesByDate} from './data.json';
+import type {Game} from '../@types';
 import SingleDaySchedule from './components/singleDaySchedule';
 import {DateTime} from 'luxon';
 import Header from './components/header';
 
-const {gamesByDate}: Schedule = data;
-
 function App() {
+  // TODO better handle serialization and enum
+  const games: Record<string, Game[]> = gamesByDate as Record<string, any[]>;
   return (
     <div className="flex w-full flex-col justify-around gap-1.5 p-2 focus-visible:outline-none md:p-3 lg:p-4">
       <Header/>
       <main className="my-3 flex flex-col gap-2 motion-safe:animate-fade-in">
-        {Object.keys(data.gamesByDate)
-          .map((date) => ({date, games: gamesByDate[date]}))
+        {Object.keys(gamesByDate)
+          .map((date) => ({date, games: games[date]}))
           .map(({date, games}) => (<SingleDaySchedule
               date={DateTime.fromISO(date).setZone('America/New_York')}
               games={games as Game[]}
