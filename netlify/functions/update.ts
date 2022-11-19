59f26fefc5ae8a3f509695c2a71c83aa46d612d9
Colby M. White <colbywhite@users.noreply.github.com>
2022-11-18 18:43:01 -0600

    debug: how does netlify handle deps

--- netlify/functions/update.ts
+++ netlify/functions/update.ts
@@ -1,8 +1,10 @@
 import type { Handler } from "@netlify/functions";
 import { schedule } from "@netlify/functions";
+import getBasketballGames from '~/lib/ingest/basketball'
 
 const update: Handler = async (event, context) => {
-  console.log("Received event:", event);
+  const games = await getBasketballGames()
+  console.log(games.length, 'games');
   return { statusCode: 200 };
 };
 
